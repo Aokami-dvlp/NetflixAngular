54cbab3fa76421e667d6338af1bcbf3592f879e7
Aokami-dvlp <emanuele.david3@gmail.com>
2021-10-29 13:00:21 +0200

    prova branching

--- Netflix/src/app/app.module.ts
+++ Netflix/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginpageComponent } from './loginpage/loginpage.component';
+import { ComponenteProvaComponent } from './homepage/componente-prova/componente-prova.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    LoginpageComponent
+    LoginpageComponent,
+    ComponenteProvaComponent
   ],
   imports: [
     BrowserModule,
