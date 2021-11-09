f9a15513df9b769e0b080be6af2e51ff830a7941
SimoPtn <ironist_92@hotmail.it>
2021-11-09 16:58:08 +0100

    first-commit

--- Netflix/src/app/app.module.ts
+++ Netflix/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule } from '@angular/forms'
+
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginpageComponent } from './loginpage/loginpage.component';
+import { LoginFormComponent } from './loginpage/core/components/login-form/login-form.component';
+import { FormBodyComponent } from './loginpage/core/components/form-body/form-body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    LoginpageComponent
+    LoginpageComponent,
+    LoginFormComponent,
+    FormBodyComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Netflix/src/app/loginpage/core/components/login-form/login-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+export class LoginFormComponent implements OnInit {
+  //@Output() submitted = new EventEmitter<Project>();//
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
