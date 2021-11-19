2df52e0e84b6e222c7ba44d9766e5259a55ff51c
Antonioverrastro <smic99@gmail.com>
2021-11-19 17:28:30 +0100

    Prova link-cards

--- Netflix/src/app/homepage/links-container/links-container.component.ts
+++ Netflix/src/app/homepage/links-container/links-container.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LinksContainerComponent implements OnInit {
 
+  links = [
+    {
+      name1: "Domande frequenti",
+      name2: "Rapporti con gli investitori",
+      name3: "Come guardare Netflix",
+      name4: "Informazioni sull'azienda",
+      name5: "Solo su Netflix"
+    },
+    {
+      name1: "Centro assistenza",
+      name2: "Opportunità di lavoro",
+      name3: "Condizioni di utilizzo",
+      name4: "Contattaci"
+    },
+    {
+      name1: "Account",
+      name2: "Riscatta carte regalo",
+      name3: "Privacy",
+      name4: "Test di velocità"
+    },
+    {
+      name1: "Media Center",
+      name2: "Acquista carte regalo",
+      name3: "Preferenze per i cookie",
+      name4: "Note legali"
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+export class Links {
+  name1!: string;
+  name2!: string;
+  name3!: string;
+  name4!: string;
+  name5?: string;
+  name6?: string;
 }
