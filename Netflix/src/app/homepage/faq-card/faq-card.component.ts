079bd5084fc65d30d5a3afb4b1e073a6af81a39a
Aokami-dvlp <emanuele.david3@gmail.com>
2021-11-20 11:16:04 +0100

    Minor fixes

--- Netflix/src/app/homepage/faq-card/faq-card.component.ts
+++ Netflix/src/app/homepage/faq-card/faq-card.component.ts
@@ -1,10 +1,13 @@
 import { Faq } from './../../models/faq';
 import { Component, Input, OnInit } from '@angular/core';
+import {NgbAccordionConfig} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'faq-card',
   templateUrl: './faq-card.component.html',
-  styleUrls: ['./faq-card.component.scss']
+  styleUrls: ['./faq-card.component.scss'],
+  providers: [NgbAccordionConfig]
 })
 export class FaqCardComponent implements OnInit {
 
@@ -13,6 +16,7 @@ export class FaqCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.faq);
   }
 
 }
